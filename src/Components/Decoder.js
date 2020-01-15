eb7dec48030a8efd75d6992a1743bfa1d76fdd54
Anton Cheng <anton@coolbitx.com>
2020-01-15 17:49:30 +0800

    Update homepage uril

---
+++ src/Components/Decoder.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+// import View from 'react-bootstrap/View'
+
+// import Container from 'react-bootstrap/Container'
+
+const Web3 = require('web3')
+const rlp = require('rlp')
+
+export default class Decoder extends Component {
+  state = {
+    mnemonic: '',
+    sumOfSeed: 0,
+  }
+
+  render() {
+    return (
+      <Container>
+        
+      </Container>
+    )
+  }
+}
+
